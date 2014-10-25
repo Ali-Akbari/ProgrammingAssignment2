9c0a319f47563edcf881cbd5dd7a05d38aca628e
ali <ali.akbari@gmail.com>
2014-10-26 10:44:23 +1100

    First and hopefully last try

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Ali:
+## I did the same style as the example
+## however I think it can be written better
+## e.g. Do set also when defining by makeCacheMatrix
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    I <- NULL
+    set <- function(y) {
+        x <<- y
+        I <<- NULL
+    }
+    get <- function() x
+    setInv <- function(Inv) I <<- Inv
+    getInv <- function() I
+    list(set = set, get = get,
+         setInv = setInv,
+         getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    I <- x$getInv()
+    if(!is.null(I)) {
+        message("getting cached data")
+        return(I)
+    }
+    data <- x$get()
+    I <- solve(data, ...)
+    x$setInv(I)
+    I
 }
