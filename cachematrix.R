1f1eeb17a045bea36f614572d048ce7fbffd305a
ali <ali.akbari@gmail.com>
2014-10-26 11:00:40 +1100

    I noticed comments are also important, so added them

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 ## Ali:
+## Following set of functions will help to reduce
+## invert matrix calculation times bu caching result and reusing it
 ## I did the same style as the example
 ## however I think it can be written better
 ## e.g. Do set also when defining by makeCacheMatrix
 
-## Write a short comment describing this function
+
+## This will initialize the object including 4 functions
+## It can set a matrix to its internal environment
+## , set its inverse, and return either of them
 
 makeCacheMatrix <- function(x = matrix()) {
     I <- NULL
@@ -23,7 +26,10 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## Following function will be used when we need 
+## to have inverse of our initialized matrix.
+## If it is the first time call, inverse will be calculated
+## otherwise only the result of last calculation will be returned
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
